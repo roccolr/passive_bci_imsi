250d9190bf8285f4e9eeffe813372c07aff587d3
roccolr <roccolorusso99@live.it>
2026-04-16 22:17:15 +0200

    implementation of a unicorn signal simulator

---
+++ STUBSCRIPT.m
@@ -0,0 +1,11 @@
+% --- PARAMETRI DI SIMULAZIONE UNICORN ---
+
+unicorn_f = 250 % hz 
+unicorn_buff_size = 25 % numero di campioni in un frame ricevuto da unicorn 
+amplitude_1 = 20e-6
+amplitude_2 = 30e-6
+amplitude_3 = 40e-6
+amplitude_4 = 50e-6
+
+
+
