7c25990aa453735fed953a0435656eeefc802351
roccolr <roccolorusso99@live.it>
2026-04-17 13:16:21 +0200

    implemented artifact remove function

--- main.m
+++ main.m
@@ -1,18 +1,27 @@
 clc
 clear
 close all
+try
+    eeglab nogui;
+catch
+    error('Assicurati che la cartella principale di EEGLAB sia nel path di MATLAB.');
+end
+eeglab_base_path = fileparts(which('eeglab'));
+addpath(genpath(fullfile(eeglab_base_path, 'plugins')));
 
 % --- PARAMETRI DI SIMULAZIONE UNICORN ---
 
-unicorn_f = 250 % hz 
-unicorn_buff_size = 25 % numero di campioni in un frame ricevuto da unicorn 
+%% 
+unicorn_f = 250 % hz
+unicorn_buff_size = 25 % numero di campioni in un frame ricevuto da unicorn
 amplitude_1 = 20e-6
 amplitude_2 = 30e-6
 amplitude_3 = 40e-6
 amplitude_4 = 50e-6
 
 calibration_duration = 60 % secondi
-campioni_per_run = calibration_duration*unicorn_f 
+campioni_per_run = calibration_duration*unicorn_f
+%% 
 
 % --- SALVATAGGIO DATI ---
 subFolder = 'vanilla_acquired_data';
@@ -23,8 +32,12 @@ end
 
 eeg_filtered_data = out.eeg_fitlered_data;
 targetPath = fullfile(pwd, subFolder, fileName);
-save(targetPath, 'eeg_data');
+save(targetPath, 'eeg_filtered_data');
 fprintf('Dati salvati correttamente in: %s\n', targetPath);
 
+%% 
 
 % --- ELABORAZIONE DATI ---
+eeg_without_artifacts = utils.remove_artifacts_eeg(targetPath, unicorn_f);
+
+%% 
